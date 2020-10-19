20df7de6620727b8e68430aac19069d2f48a051d
Joseph Costello <joseph.costello.17@cnu.edu>
2020-10-18 22:13:40 -0400

    Initial Commit

---
+++ DateTime/wxmView.hpp
@@ -0,0 +1,18 @@
+//
+//  wxmView.hpp
+//  DateTime
+//
+//  Created by Joseph Costello on 10/18/20.
+//
+
+#include <wx/wx.h>
+
+class wxmView : public wxFrame {
+public:
+    wxmView();
+    virtual ~wxmView();
+protected:
+    
+private:
+    DECLARE_EVENT_TABLE()
+};
