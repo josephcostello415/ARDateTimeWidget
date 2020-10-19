1fd8e0023a08ab36a077311cdf22feb58293c406
Joseph Costello <joseph.costello.17@cnu.edu>
2020-10-19 19:39:09 -0400

    initial commit

--- DateTime/wxmView.cpp
+++ DateTime/wxmView.cpp
@@ -18,26 +18,60 @@
 
 
 
-#define frameWidth 400
-#define frameHeight 400
+#define frameWidth 600
+#define frameHeight 50
 
 
+enum {
+    ID_VALUE_TIMER = 10001,
+    
+};
+
+//Constructor
 wxmView::wxmView()
 : wxFrame(NULL, wxID_ANY, "DateTime")
 {
+    //Set background color and size
+    this->wxWindow::SetBackgroundColour(* wxGREEN);
     this->SetSize(frameWidth, frameHeight);
+    //Set min and max size (window cannot be resized)
     SetMinSize(GetSize());
     SetMaxSize(GetSize());
     
-    // add bottom status bar
-    CreateStatusBar();
-    SetStatusText( "Status...!!!" );
+    //Initialize and start timer with event intervals of 100 milliseconds
+    m_timer = new wxTimer(this, ID_VALUE_TIMER);
+    m_timer->Start(100);
     
-    // center application frame after opening it
+    //Create font for text
+    wxFont myFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+    //Get current time
+    wxDateTime now  = wxDateTime::Now();
+    //Initialize text for string
+    time = new wxStaticText(this, wxID_ANY, now.Format("%c", wxDateTime::EST).c_str(), wxPoint(1000,500), wxSize(300,50));
+    //Set font of text
+    time->SetFont(myFont);
+    
+    //Show application
+    this->Refresh();
+    //Center frame on window
     this->Center();
 }
 
+//Helper function for updating time when timer event is intialized
+void wxmView::OnTimer(wxTimerEvent &event){
+    //Get current time
+    wxDateTime current = wxDateTime::Now();
+    //Update text
+    time->SetLabel(current.Format("%c", wxDateTime::EST).c_str());
+    //End function
+    event.Skip();
+}
+
+//Destructor
 wxmView::~wxmView() {}
 
+
 BEGIN_EVENT_TABLE(wxmView, wxFrame)
+//Listen for timer event and run OnTimer function
+EVT_TIMER(ID_VALUE_TIMER, wxmView::OnTimer)
 wxEND_EVENT_TABLE()
--- DateTime/wxmView.hpp
+++ DateTime/wxmView.hpp
@@ -7,12 +7,20 @@
 
 #include <wx/wx.h>
 
+
 class wxmView : public wxFrame {
 public:
     wxmView();
     virtual ~wxmView();
+    //Text for displaying time
+    wxStaticText *time = nullptr;
+    //Timer for updating time
+    wxTimer *m_timer = nullptr;
+    //Function for updating text for time display
+    void OnTimer(wxTimerEvent &event);
 protected:
     
 private:
+    //Event table for timer
     DECLARE_EVENT_TABLE()
 };
